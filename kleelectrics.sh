a3cae5e4c2394ce1e764baab669ea13cf1aaf3eb
Anja Kleebaum <anja.kleebaum@kleebaum.de>
2014-05-17 23:14:33 +0200

    variable directory for shell script

--- kleelectrics.sh
+++ kleelectrics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+_script="$(readlink -f ${BASH_SOURCE[0]})"
+_base="$(dirname $_script)"
 
-{ 
-    R -f kleelectrics/kleelectrics.r
-} || {
-    exec R -f kleelectrics.r
-}
+echo $_base
+ 
+R -f "$_base/kleelectrics.r"
